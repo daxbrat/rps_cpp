e1ac122bb1fd090eb09af361d1a6994f4f0b1d68
Ricio Dax Brathwaite <dax.dacosta@gmail.com>
2023-08-23 18:24:53 -0600

    complete computerSelection code

--- main.cpp
+++ main.cpp
@@ -2,27 +2,36 @@
 
 int main()
 {
-	int computerSelection{};
+	string computerSelection{};
 	int playerSelection{};
 
 	// ------------------------Random number generator
-	std::random_device rd;
-	std::seed_seq ss{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() }; // get 8 integers of random numbers from std::random_device for our seed
-	std::mt19937 mt{ ss }; // initialize our Mersenne Twister with the std::seed_seq
+	random_device rd;
+	seed_seq ss{ rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd() }; // get 8 integers of random numbers from std::random_device for our seed
+	mt19937 mt{ ss }; // initialize our Mersenne Twister with the std::seed_seq
 
 	// Create a reusable random number generator that generates uniform numbers between 1 and 6
-	std::uniform_int_distribution die6{ 1, 3 }; // for C++14, use std::uniform_int_distribution<> die6{ 1, 6 };
+	uniform_int_distribution die6{ 1, 3 }; // for C++14, use std::uniform_int_distribution<> die6{ 1, 6 };
 
 	// Print a bunch of random numbers
-	for (int count{ 1 }; count <= 1; ++count)
+	/* for (int count{1}; count <= 1; ++count)
 	{
-		std::cout << die6(mt) << '\t' << std::endl; // generate a roll of the die here
-		system("pause>0");
+		// cout << die6(mt) << '\t' << endl; // generate a roll of the die here
 
 		// If we've printed 10 numbers, start a new row
 		//	if (count % 10 == 0)
 		//	std::cout << '\n';
-	}
+	} */
 	// ------------------------------------------------------------------------------------
-
+	if (die6(mt) == 1) {
+		computerSelection = "Rock";
+	}
+	else if (die6(mt) == 2) {
+			computerSelection = "Paper";
+		
+	} else {
+	 computerSelection = "Scissors";
+	}
+	cout << computerSelection << '\n';
+	
 }
